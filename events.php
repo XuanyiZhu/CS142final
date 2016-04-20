93ca6f9ef489159cbef106a628d61fb8e50781e1
Neal zhu <Neal zhu>
2016-04-20 10:11:37 -0400

    get files from emily and peter

---
+++ events.php
@@ -0,0 +1,30 @@
+<?php
+include "top.php"; 
+?>
+<h1> Events</h1>
+<h3>A list of past and upcoming IEEE events.</h3>
+
+<div class="event">
+<h2>Student Professional Awarness Conference (S-PAC)</h2>
+<p> An annual conference focused on career development with a national
+    speaker and a panel of professionals. Participants also have the chance
+    to talk to each company and provide a resume to them.</p>
+<p><a class="div-button" href="/octocopter.php">View details</a></p>
+</div>
+
+
+
+<div class="event">
+<h2>EE Student Faculty Mingler</h2>
+<p> An opportunity for EE students of all years to network with graduates and faculty.
+    Food and refreshments will be served. </p>
+<p><a class="div-button" href="octocopter.php">View details</a></p>
+</div>
+
+
+
+
+<?php include "footer.php"; ?>
+
+</body>
+
---
+++ projects.php
@@ -0,0 +1,27 @@
+<?php
+include "top.php"; 
+?>
+<h1> Current Projects</h1>
+<h3>Take a quick look at some of the projects we're currently working on and things you can expect to see in the future! </h3>
+
+<div class="project">
+<h2>Octocopter</h2>
+<p> this is our project this is our project bla bla bla.this is our project this is our project bla bla blab.
+this is our project this is our project bla bla bla.this is our project this is our project bla bla blab.
+this is our project this is our project bla bla bla.this is our project this is our project bla bla bla.</p>
+<p><a href="Projects/octocopter.php" class="div-button">View details</a></p>
+</div>
+
+<div class="project">
+<h2>Project 2</h2>
+<p> this is our project this is our project bla bla bla.this is our project this is our project bla bla blab.
+this is our project this is our project bla bla bla.this is our project this is our project bla bla blab.
+this is our project this is our project bla bla bla.this is our project this is our project bla bla bla.</p>
+<p><a href="Projects/octocopter.php"  class="div-button" >View details</a></p>
+</div>
+
+
+<?php include "footer.php"; ?>
+
+</body>
+
