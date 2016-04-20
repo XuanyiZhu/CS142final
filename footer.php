c13ed6ecbfdd218069661c0aa2a53105de9670ec
XuanyiZhu <xzhu2@uvm.edu>
2016-04-20 10:05:19 -0400

    Added files via upload

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
-<div class="fb-share-button" data-href="https://www.facebook.com/UVM.IEEE/?fref=nf" 
-     data-layout="button" data-mobile-iframe="true"></div>
+<footer>
+    <div class="fb-share-button" data-href="https://www.facebook.com/UVM.IEEE/?fref=nf" 
+         data-layout="button" data-mobile-iframe="true"></div>
+</footer>
 </html>
