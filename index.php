1923dff2c4533303d39668c2828313efebae4287
Neal zhu <Neal zhu>
2016-04-20 00:56:36 -0400

    finish the home page image slide

--- index.php
+++ index.php
@@ -1,42 +1,62 @@
 <?php
 include 'top.php';
 ?>
+<!--########################slide pictures##################################-->
+<div class="homeSlide">
+    <img class="slides" src="img/1.png" alt="">
+    <img class="slides" src="img/2.png" alt="">
+    <img class="slides" src="img/3.png" alt="">
 
-
-
-    <div class="homeSlides">
-        <style>
-            .slides {display:none;}
-        </style>
-        <img class="slides" src="img/1.png">
-        <img class="slides" src="img/2.png">
-        <img class="slides" src="img/3.png">
-        <a class="btn-left" onclick="plusDivs(-1)">❮</a>
-        <a class="btn-right" onclick="plusDivs(1)">❯</a>
-        <span class="dot" onclick="currentDiv(1)"></span>
-        <span class="dot" onclick="currentDiv(2)"></span>
-        <span class="dot" onclick="currentDiv(3)"></span>
+    <div class="leftArrow" onclick="plusDivs(-1)"><img class="left-arrow" src="img/left-arrow.png" alt="left arrow"></div>
+    <div class="rightArrow" onclick="plusDivs(1)"><img class="right-arrow" src="img/right-arrow.png" alt="right arrow"></div>
+    <div class="center section text-white display-bottomleft" style="width:100%">
+        <span class="badge demo border transparent hover-white" onclick="currentDiv(1)"></span>
+        <span class="badge demo border transparent hover-white" onclick="currentDiv(2)"></span>
+        <span class="badge demo border transparent hover-white" onclick="currentDiv(3)"></span>
     </div>
-    <script>
-            var index = 0;
-            carousel();
+</div>
+<script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
 
-            function carousel() {
-                var i;
-                var x = document.getElementsByClassName("slides");
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.display = "none";
-                }
-                index++;
-                if (index > x.length) {
-                    index = 1;
-                }
-                x[index - 1].style.display = "block";
-                setTimeout(carousel, 4000);
-            }
-        </script>
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    function currentDiv(n) {
+        showDivs(slideIndex = n);
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("slides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = x.length;
+        }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-white", "");
+        }
+        x[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " w3-white";
+    }
+</script>
+<!--######################## end slide pictures ##################################-->
+<article>
+    <h1>Welcome to the UVM IEEE</h1>
+    <p>The Institute of Electrical and Electronics Engineers at the University of 
+        Vermont is a resource that provides support for electrical engineering 
+        students outside the classroom. We conduct a lecture series throughout 
+        the academic year as well facilitate student projects and sponsor career 
+        networking events.</p>
+</article>
 </body>
-
 <?php
 include 'footer.php';
 ?>
--- top.php
+++ top.php
@@ -3,27 +3,29 @@
     <head>
         <title>UVM IEEE</title>
         <meta name="author" content="Maxfield Valentine Davis, Emily Smith, Neal Zhu">
-        <meta description="The Institute of Electrical and Electronics Engineers at the
-              University of Vermont is a resource that provides support for electrical 
-              engineering students outside the classroom.">
+
         <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
         <link rel='icon' type='img/x-icon' href='icon.ico' property='stylesheet'>
         <link rel='stylesheet' href='stylesheet.css' type='text/css' media='screen'>
-
+<!--        <meta description="The Institute of Electrical and Electronics Engineers at the
+              University of Vermont is a resource that provides support for electrical 
+              engineering students outside the classroom.">-->
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
         <![endif]-->
 
         <!--########################slide pictures##################################-->
-
-
-        <!--######################## end slide pictures##################################-->
-
-
+        <style>
+            .slides {display:none}
+            .leftArrow, .rightArrow, .badge {cursor:pointer}
+            .badge {height:13px;width:13px;padding:0}
+        </style>
+        <!--######################## end slide pictures ##################################-->
 
     </head>
 
     <body>
+        <!--######################## facebook ##################################-->
         <div id="fb-root"></div>
         <script>(function (d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
@@ -34,29 +36,4 @@
                 js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
                 fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));</script>
-
-        <script>
-            var slideIndex = 1;
-            showDivs(slideIndex);
-
-            function plusDivs(n) {
-                showDivs(slideIndex += n);
-            }
-
-            function showDivs(n) {
-                var i;
-                var x = document.getElementsByClassName("slides");
-                if (n > x.length) {
-                    slideIndex = 1;
-                }
-                if (n < 1) {
-                    slideIndex = x.length;
-                }
-                ;
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.display = "none";
-                }
-                x[slideIndex - 1].style.display = "block";
-            }
-        </script>
-        
+        <!--######################## end facebook##################################-->
