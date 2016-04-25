7d2bed72bbe9d3ff7a84dd5b7e31af36fdc4d5cf
esmith54 <esmith54@uvm.edu>
2016-04-25 16:30:57 -0400

    Added files via upload

---
+++ officers.php
@@ -0,0 +1,91 @@
+<?php
+include "top.php"; 
+include "nav.php";
+?>
+
+<h1>Officers</h1>
+
+
+
+<div class="container">
+        	<div class="containter marketing">
+            	<div class="row featurette">
+                	<div class="col-md-7">
+                    	<h2 class="featurette-heading">
+                        President 
+                     	</h2>
+                        <p class="lead">
+                        Kaitlyn Mayberry is a senior Electrical Engineering major. She is particularly interested in renewable energy. This past summer Kaitlyn interned with GE Aviation in Rutland, VT, where she contributed to quality improvement and facilities planning. At UVM she plays the flute and piccolo in the Symphony Orchestra and is also a trip leader for Alternative Spring Break. In her free time, Kaitlyn enjoys biking, hiking, Bikram yoga, and cooking.
+                        </p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-circle img-reponsive" src="img/president.jpg" alt="" />
+                    </div>
+                </div><!-- row featurette -->
+                <hr class="featurette-divider" />
+                <div class="row featurette">
+                	<div class="col-md-5">
+                    	<img class="img-circle img-reponsive" src="img/vp.jpg" alt="" />
+                    </div>
+                    <div class="col-md-7">
+                    	<h2 class="featurette-heading">
+                        Vice President
+                     	</h2>
+                        <p class="lead">
+                        Connor Yamada is a senior majoring in Electrical Engineering. He has interests in embedded systems, programming, and financial services. In the past he has interned at AllianceBernstein, Noise New York, and JWT New York. In his free time Connor likes to fence, play tennis, and go out exploring with his camera.
+                        </p>
+                    </div>
+                </div><!-- row featurette -->	
+                <hr class="featurette-divider" />	
+                <div class="row featurette">
+                	<div class="col-md-7">
+                    	<h2 class="featurette-heading">
+                        Treasurer 
+                     	</h2>
+                        <p class="lead">
+                        Daniel Mepham is a junior Electrical Engineering student from Sudbury, Massachusetts. His interests/activities include hiking, skiing, and generally enjoying the Vermont outdoors. During his time at UVM, he has also competed on the club basketball team and the crew team.
+                        </p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-circle img-reponsive" src="img/treasurer.png" alt="" />
+                    </div>
+                </div><!-- row featurette -->
+                 <hr class="featurette-divider" />
+                <div class="row featurette">
+                	<div class="col-md-5">
+                            <img class="img-circle img-reponsive" src="img/eventco.jpg" alt="caitlin" />
+                    </div>
+                    <div class="col-md-7">
+                    	<h2 class="featurette-heading">
+                        Event Coordinator
+                     	</h2>
+                        <p class="lead">
+                        Caitlin Bliss is currently a senior electrical engineering major. She became interested in engineering because math and science, two of her favorite subjects, were such a large part of engineering. The past two years she has interned at Green Mountain Power in Vermont and fell in love with the utilities side of engineering. Outside of school she loves to ski, hangout with friends and eat. She considers herself addicted to food. 
+                        </p>
+                    </div>
+                </div><!-- row featurette -->
+                <hr class="featurette-divider" />	
+                <div class="row featurette">
+                	<div class="col-md-7">
+                    	<h2 class="featurette-heading">
+                        Academic Advisor
+                     	</h2>
+                        <p class="lead">
+                        Tian Xia received the B.E. degree in electrical engineering from the Huazhong University of Science and Technology, Wuhan, China, in 1994, the M.S. degree from the Nanjing University of Posts and Telecommunications, Nanjing, China, in 2000, and the Ph.D. degree in electrical and computer engineering from the University of Rhode Island, Kingston, in 2003. During the summers of 2002 and 2003, he did his internship in IBM T. J. Watson Research Center, Yorktown Heights, NY. He is now an Associate Professor with the School of Engineering, University of Vermont.  His current research interests are in ground penetrating radar, mixed-signal and RF circuit design and test. He is the recipient of the Best Paper Award ofIEEE North Atlantic Test Workshop 2005, the Best Paper Award of IEEE System on Chip Conference 2012. Dr. Xia received the IBM Faculty Award in 2005, 2006 and 2008. He was the program chair of IEEE North Atlantic Test Workshop 2011, 2012, 2013. Guest editor of Journal of Electronic Test: Theory and Application (JETTA) 2013, and Associate editor of Journal of Circuits, Systems, and Computers (JCSC) (2013- present). He is a senior member of IEEE.
+                        </p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="img-circle img-reponsive" src="img/advisor.jpg" alt="" />
+                    </div>
+            </div>
+        </div>	
+    
+
+
+</body>
+
+
+<?php
+include 'footer.php';
+?>
+
